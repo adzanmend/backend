e3656e6a878b511bec1765cc3d21a82fc671aeb0
adzanmend <adzanmendrofa3@gmail.com>
2021-10-08 15:04:31 +0700

    coba

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4c54adefd1471b12ac5c52c5f14ccac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ffd45cdb-43b4-4507-b524-47e516bb1c11'
+};
